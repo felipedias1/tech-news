b3f584a14a6cd630c21bc87052a94bcc10584f54
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-19 16:34:03 -0300

    Feat: build function tthe 5 most popular news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news_find = []
+    for new in news:
+        new["popularity"] = new['comments_count'] + new['shares_count']
+    news_sorted = sorted(
+        news, key=lambda i: (i["popularity"], i["title"]), reverse=True
+    )
+    for new in news_sorted[:5]:
+        news_find.append((new["title"], new["url"]))
+    return news_find
 
 
 # Requisito 11
